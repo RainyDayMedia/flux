848e2c4bc7cbf73e5428313fe0733c63a5680473
Ed Heltzel <ginfuru@users.noreply.github.com>
2016-07-15 14:32:43 -0400

    added extra line to index

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ get_header(); ?>
 	<section class="bg-gray dark typography text-center">
         <div class="six columns push-three padded-5">
             <h1>You've done it! Flux lives!</h1>
+            <p>Now get to work making something great.</p>
         </div>
     </section>
 
